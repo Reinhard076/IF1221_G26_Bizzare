9ddcd8ea84b002cf9e9615434ea3a4d662329336
ExcellActIV <excelltjt@gmail.com>
2026-05-11 20:06:30 +0700

    chore: initialize main.pl and state.pl

--- src/main.pl
+++ src/main.pl
@@ -1,1 +1,18 @@
-kartu(ya).
+% Integrasi semua file modul
+:- include('state.pl').
+:- include('deck.pl').
+:- include('game_setup.pl').
+:- include('player_turn.pl').
+:- include('game_info.pl').
+
+start :-
+    write('Selamat datang di Game UNI!'), nl,
+    write('Ketik "startGame." untuk memulai permainan.'), nl.
+
+mainkanKartu(N) :-
+    is_game_started(true),
+    !,
+    eksekusi_mainkan(N).
+
+mainkanKartu(_) :-
+    write('Error: Permainan belum dimulai! Gunakan startGame. dulu.'), nl.
