90fba51e782d36ebcc2a520a46fc4aaab3b9cf58
Reinhard076 <13525076@std.stei.itb.ac.id>
2026-05-11 23:04:03 +0700

    feat: game setup

---
+++ src/game_setup.pl
@@ -0,0 +1,77 @@
+:- include('state.pl').
+
+startGame :-
+    is_game_started(true), !,
+    write('Permainan sudah dimulai').
+
+startGame :-
+    assertz(is_game_started(true)),
+    write('Masukkan jumlah pemain: '),
+    read(Player),
+    valid_Player(Player),
+    read_PlayerNames(Player, 1),
+    randomizer,
+    !.
+
+
+valid_Player(Player) :-
+    Player >= 2,
+    Player =< 4,
+    assertz(jumlah_pemain(Player)),
+    !.
+
+valid_Player(_) :-
+    write('Mohon masukkan angka antara 2-4.'),
+    nl,
+    write('Masukkan jumlah pemain: '),
+    read(NewPlayer),
+    valid_Player(NewPlayer),
+    !.
+
+read_PlayerNames(All, Count) :-
+    X is Count,
+    All < X,
+    !.
+
+read_PlayerNames(All, Count) :-
+    format('Masukkan nama pemain ~d: ', [Count]),
+    read(Name),
+    X is Count + 1,
+    addNewName(Name),
+    read_PlayerNames(All, X),
+    !.
+
+addNewName(Name) :-
+    daftar_pemain(Name),
+    write('Nama sudah digunakan. Masukkan nama lain : '),
+    read(NewName),
+    !,
+    addNewName(NewName).
+
+addNewName(Name) :-
+    assertz(daftar_pemain(Name)),
+    !.
+
+randomizer :-
+    findall(Nama, daftar_pemain(Nama), X),
+    jumlah_pemain(Count),
+    random_List(X, Count, Y),
+    retractall(daftar_pemain(_)),
+    assertz(daftar_pemain(Y)),
+    !.
+
+random_List([], _, []) :- !.
+
+random_List(Before, Count, [New|After]) :-
+    Count1 is Count + 1,
+    random(1, Count1, Rand),
+    pick_atI(Before, Rand, New, Rest),
+    C1 is Count - 1,
+    random_List(Rest, C1, After),
+    !.
+
+pick_atI([H|T], 1, H, T) :- !.
+pick_atI([H|T], I, X, [H|Rest]) :-
+    I1 is I - 1,
+    pick_atI(T, I1, X, Rest),
+    !.
