f74cbd3a3b8123f18401dcda1c592935031fee5f
Zyy <fahrezyfitriansyah25@gmail.com>
2026-05-12 10:20:00 +0700

    Update deck.pl

--- src/deck.pl
+++ src/deck.pl
@@ -1,3 +1,3 @@
-shuffle_deck(Dek):-
-bagi_kartu(Dek,Player):-
-discard_pile():-
+:- dynamic(shuffle_dek/1).  %shuffle_dek(Dek)       mengacak kartu dalam dek
+:- dynamic(bagi_kartu/2).   %bagi_kartu(Dek/Pemain) memberi kartu untuk pemain 
+:- dynamic(discard_pile/0). %discard_card()         membuka 1 kartu angka sebagai kartu awal di meja
