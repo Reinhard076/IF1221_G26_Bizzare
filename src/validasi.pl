3f4a1cf7c393750bb7169ef70093255497b528c4
ExcellActIV <excelltjt@gmail.com>
2026-05-18 23:43:41 +0700

    feat: add validation check to cards that are being played and integrate it to player_turn.pl

---
+++ src/validasi.pl
@@ -0,0 +1,49 @@
+validasi_kartu(_, kartu(Warna, Angka)) :-
+    integer(Angka),
+    warna_aktif(WarnaAktif),
+    discard_top(kartu(_, JenisTop)),
+    (Warna == WarnaAktif ; Angka == JenisTop),
+    !.
+
+validasi_kartu(_, kartu(Warna, skip)) :-
+    warna_aktif(WarnaAktif),
+    discard_top(kartu(_, JenisTop)),
+    (Warna == WarnaAktif ; JenisTop == skip),
+    !.
+
+validasi_kartu(_, kartu(Warna, reverse)) :-
+    warna_aktif(WarnaAktif),
+    discard_top(kartu(_, JenisTop)),
+    ( Warna == WarnaAktif ; JenisTop == reverse ),
+    !.
+
+validasi_kartu(_, kartu(Warna, draw_two)) :-
+    warna_aktif(WarnaAktif),
+    discard_top(kartu(_, JenisTop)),
+    JenisTop \== draw_two,
+    Warna == WarnaAktif,
+    !.
+
+validasi_kartu(_, kartu(hitam, wild)) :-
+    discard_top(kartu(_, JenisTop)),
+    JenisTop \== wild,
+    !.
+
+validasi_kartu(Pemain, kartu(hitam, wild_draw_four)) :-
+    discard_top(kartu(_, JenisTop)),
+    JenisTop \== wild_draw_four,
+    warna_aktif(WarnaAktif),
+    tangan_pemain(Pemain, Tangan),
+    \+ punya_kartu_cocok(Tangan, WarnaAktif, JenisTop),
+    !.
+
+validasi_kartu(_, kartu(hitam, mimic)) :-
+    !.
+
+punya_kartu_cocok([Kartu|_], WarnaAktif, JenisTop) :-
+    bisa_cocok(Kartu, WarnaAktif, JenisTop), !.
+punya_kartu_cocok([_|TanganLain], WarnaAktif, JenisTop) :-
+    punya_kartu_cocok(TanganLain, WarnaAktif, JenisTop).
+
+bisa_cocok(kartu(Warna, _), WarnaAktif, _) :- Warna == WarnaAktif.
+bisa_cocok(kartu(_, Angka), _, JenisTop) :- integer(Angka), Angka == JenisTop.
